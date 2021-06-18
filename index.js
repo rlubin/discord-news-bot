5da1ecf68dd53760c89e1947cc0a4516565d95eb
Ryan Lubin <ryan.lubin1@gmail.com>
2021-06-18 13:16:24 -0400

    added a functionality to allow 'alerts1' command to get stock market alerts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const NewsAPI = require('newsapi')
 const newsapi = new NewsAPI(process.env.NEWS_API)
+const fetch = require('node-fetch')
 
 client.once('ready', () => {
 	console.log('ready')
@@ -17,6 +18,16 @@ client.on('message', (message) => {
 		checkForNews().then((data) => {
 			for (article of data) {
 				message.channel.send(article.url).catch((error) => console.log(error))
+			}
+		})
+	}
+	if (message.content === 'alerts1') {
+		getPercentVolumeAbove(7, 150000).then((tickers) => {
+			for (ticker of tickers) {
+				// console.log(ticker)
+				message.channel
+					.send(`${ticker['symbol']} - ${ticker['name']}`)
+					.catch((error) => console.log(error))
 			}
 		})
 	}
@@ -38,29 +49,81 @@ const checkForNews = async () => {
 			page: 2,
 		})
 		.then((response) => {
-			// console.log(response)
-			// console.log(response.articles)
-			// console.log(response.articles[0].url)
-			// console.log(client)
-			// console.log(client.channels.cache)
-			// client.channels.cache.get('854714038457204786').send('found')
-			// client.channels.cache.get('news').send(url)
-			// return response.articles[0].url
-			// console.log(typeof response)
 			return response.articles
-			/*
-			{
-				status: "ok",
-				articles: [...]
-			}
-		*/
 		})
 		.catch((error) => console.log(error))
 }
 
-// runs every 15 minutes
-// setInterval(checkForNews, 900000)
+const getPercentVolumeAbove = async (percent, volume) => {
+	const tickers = await getTickersTSX()
+	// for (ticker of tickers) {
+	// 	console.log(ticker)
+	// }
+	// console.log(tickers)
+	const tickers_above = await getPriceChangeVolumeLarger(
+		tickers,
+		percent,
+		volume
+	)
+	// console.log(tickers_above)
+	// return ['ABC.TO', 'BCD.TO']
+	return tickers_above
+}
 
-// checkForNews()
+const getTickersTSX = async () => {
+	const url = 'https://financialmodelingprep.com/api/v3'
+	const fmp_key = process.env.FMP_API
+	return fetch(`${url}/symbol/available-tsx?apikey=${fmp_key}`)
+		.then((res) => res.json())
+		.then((tickers) => {
+			let tickers_list = []
+			for (ticker of tickers) {
+				// console.log(ticker['symbol'])
+				tickers_list.push({ symbol: ticker['symbol'], name: ticker['name'] })
+			}
+			return tickers_list
+		})
+		.catch((error) => console.log(error))
+}
+
+const getPriceChangeVolumeLarger = async (tickers, percent, volume) => {
+	const matching_tickers = []
+	for (ticker of tickers) {
+		await sleep(200)
+		data = await getDailyPriceData(ticker['symbol'])
+		// console.log(data[0]['close'])
+		// console.log(data[1]['close'])
+		// console.log(data[0]['volume'])
+		todays_close = data[0]['close']
+		yestersdays_close = data[1]['close']
+		todays_volume = data[0]['volume']
+		// console.log(1.0 + percent * 0.01)
+		// console.log(yestersdays_close * (1.0 + percent * 0.01))
+		// console.log(todays_close)
+		if (
+			todays_close > yestersdays_close * (1.0 + percent * 0.01) &&
+			todays_volume > volume
+		) {
+			matching_tickers.push(ticker)
+		}
+		// console.log(matching_tickers)
+	}
+	return matching_tickers
+}
+
+const sleep = async (ms) => {
+	return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+const getDailyPriceData = async (ticker) => {
+	const url = 'https://financialmodelingprep.com/api/v3'
+	const fmp_key = process.env.FMP_API
+	return fetch(`${url}/historical-price-full/${ticker}?apikey=${fmp_key}`)
+		.then((res) => res.json())
+		.then((price_data) => {
+			return price_data['historical']
+		})
+		.catch((error) => console.log(error))
+}
 
 client.login(process.env.TOKEN)
